fc73ad208abfb5beb1c35b25642bd299a33f926d
Ryan Bonte <ryan.bonte@gmail.com>
2013-06-26 01:06:54 +0200

    added length validation to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ActiveRecord::Base
   belongs_to :teacher
 
   validates_presence_of :content, message: 'Mag niet leeg zijn.'
+  validates_length_of :content, maximum: 255, message: 'Mag niet langer dan 255 tekens zijn.'
 end
